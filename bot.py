1ee539b2e5f8a5b75bfb1abb07e4a4584f169e89
Olaf <adams.olaf@gmail.com>
2023-09-24 14:18:39 +0200

    Keep track of corvas prime mentions per member

--- bot.py
+++ bot.py
@@ -4,9 +4,9 @@ import responses
 URL = "https://discord.com/api/oauth2/authorize?client_id=1150433259788968016&permissions=414464698368&scope=bot"
 TOKEN = ""
 
-async def sendMessage(message, userMessage, serverID):
+async def sendMessage(message, userMessage, serverID, user):
     try:
-        response = responses.handleResponse(userMessage, serverID)
+        response = responses.handleResponse(userMessage, serverID, user, message.guild.name)
         if (response != None):
             await message.channel.send(response)
     except Exception as e:
@@ -25,5 +25,6 @@ def runBot():
             return
         user_message = str(message.content)
         serverID = message.guild.id
-        await sendMessage(message, user_message, serverID)
+        user = str(message.author)
+        await sendMessage(message, user_message, serverID, user)
     client.run(TOKEN)
--- responses.py
+++ responses.py
@@ -1,11 +1,23 @@
 import time
+import mysql.connector
 
 keyWords = ["corvas", "prime"]
 servers = {}
 
-timeoutAmount = 10
+timeoutAmount = 60
 
-def handleResponse(message, serverID) -> str:
+def createConnection():
+    connection = mysql.connector.connect(
+            host="",
+            port="",
+            user="",
+            password="",
+            database=""
+        )
+    
+    return connection
+
+def handleResponse(message, serverID, user, serverName) -> str:
     msg = message.lower()
     curServer = serverID
 
@@ -20,10 +32,21 @@ def handleResponse(message, serverID) -> str:
 
     # Check if the message contains the right keywords.
     if (all([x in msg for x in keyWords])):
-        print("Corvas Prime mentioned!")
+        print(f"Corvas Prime mentioned by {user}!")
+        connection = createConnection()
+        cursor = connection.cursor()
+        pStatement = """IF NOT EXISTS(SELECT 1 FROM primers WHERE dcName = %s AND dcServer = %s)
+                        THEN INSERT INTO primers (dcName, primeMentions, dcServer) VALUES (%s, 1, %s);
+                        ELSE UPDATE primers
+                        SET primeMentions = primeMentions + 1
+                        WHERE dcName = %s AND dcServer = %s;
+                        END IF"""
+        sParams = (user, serverName, user, serverName, user, serverName)
+        cursor.execute(pStatement, sParams)
+        connection.commit()
+        cursor.close()
+        connection.close()
         if (curTime - lastTime >= timeoutAmount):
             servers[curServer] = curTime
             return "https://www.youtube.com/watch?v=VvGG9xdmAwE"
     return None
-    
-
